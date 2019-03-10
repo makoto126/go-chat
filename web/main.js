10b1dfd8921e7e6b0294b6879c962e51ce5db95a
makoto126 <makoto126@126.com>
2019-03-11 01:27:51 +0800

    Update main.js

--- web/main.js
+++ web/main.js
@@ -1,9 +1,6 @@
 var conn = new WebSocket('ws://localhost:12345/ws');
 var messages = [];
 
-//Javascript's atob to decode base64 doesn't properly decode utf-8 strings, so found this at
-//https://stackoverflow.com/questions/30106476/using-javascripts-atob-to-decode-base64-doesnt-properly-decode-utf-8-strings
-
 conn.onmessage = function (e) {
     m = JSON.parse(e.data);
     console.log(m);
@@ -21,6 +18,8 @@ var app = new Vue({
             conn.send(this.text);
             this.text = '';
         },
+        //Javascript's atob to decode base64 doesn't properly decode utf-8 strings, so found this at
+        //https://stackoverflow.com/questions/30106476/using-javascripts-atob-to-decode-base64-doesnt-properly-decode-utf-8-strings
         b64DecodeUnicode: function (str) {
             return decodeURIComponent(atob(str).split('').map(function (c) {
                 return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
