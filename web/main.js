ea732a483d52e72cde655661c2d662a4ed16aff7
makoto126 <makoto126@126.com>
2019-03-17 16:57:30 +0800

    1

--- web/main.js
+++ web/main.js
@@ -1,19 +1,22 @@
 var conn = new WebSocket('ws://localhost:12345/ws');
 var messages = [];
-
+conn.onerror = function () {
+    alert('connection failed!');
+}
 conn.onmessage = function (e) {
     m = JSON.parse(e.data);
-    console.log(m);
     messages.push(m);
 }
 
 var app = new Vue({
     el: '#app',
+
     data: {
+        messages: messages,
         text: '',
         placeholder: 'Firstly, please input your name.',
-        messages: messages
     },
+
     methods: {
         send: function () {
             conn.send(this.text);
@@ -22,3 +25,17 @@ var app = new Vue({
         },
     }
 })
+
+
+var input = document.getElementById("input");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("button").click();
+  }
+});
